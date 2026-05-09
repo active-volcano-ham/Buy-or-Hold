6d72d1543fe608161163dbb8de0155ba5264d03b
active-volcano-ham <hamham8218@gmail.com>
2026-05-09 13:28:06 +0900

    Initial commit with assetlinks

---
+++ app/src/main/java/app/lovable/discount_detective_bot/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.lovable.discount_detective_bot.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
